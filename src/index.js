e26135ddd0c726c3d152bd7e9ca941088a02e3a0
giplash <anashkinviktar@gmail.com>
2018-07-28 15:53:36 +0300

    first version

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './containers/App';
+import reducers from './reducers';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
-const App = () => (
-  <h1>Hello world!</h1>
-)
+const store = createStore(
+  reducers,
+);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
